78833c04f9f20d4f8d21f084fb7e53f5fe30a18b
saifrehman <s4saif.121@gmail.com>
2018-08-22 21:39:00 +0400

    added frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ROUTING } from './app.routing';
+import { AppComponent } from './app.component';
+import { TrainComponent } from './train/train.component';
+import { RecogniseComponent } from './recognise/recognise.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { TrainService } from './train/train.component.service';
+import { RecogniseService } from './recognise/recognise.component.service';
+import {ToastModule} from 'ng2-toastr/ng2-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TrainComponent,
+    RecogniseComponent
+    
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    ToastModule.forRoot(),
+    BrowserModule,
+    ROUTING,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [TrainService,RecogniseService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
